d20e39b7f8f9d01e0063297a26c84720ea908ff2
mohamed-bahaj <Mohamed.BAHAJ@student.umons.ac.be>
2017-11-06 15:35:48 +0100

    Exercice 2

--- src/main/java/exercise2/Exercise2.java
+++ src/main/java/exercise2/Exercise2.java
@@ -1,21 +1,27 @@
 package exercise2;
 
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDate;
 
 public class Exercise2 {
-	public static void save(Classroom classroom, Path filePath) {
-
+	public static void save(Classroom classroom, Path filePath) throws IOException{
+		try (OutputStream out = Files.newOutputStream(filePath)){
+			ObjectOutput oos = new ObjectOutputStream(out);
+			oos.writeObject(classroom);
+		}
 	}
 
-	public static Classroom load(Path filePath) {
-		return null;
+	public static Classroom load(Path filePath) throws IOException, ClassNotFoundException {
+		try(InputStream in = Files.newInputStream(filePath)){
+			ObjectInput ois = new ObjectInputStream(in);
+			return (Classroom) ois.readObject();
+		}
 	}
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, ClassNotFoundException{
 		Teacher teacher = new Teacher("Claire", "Barnett",
 			LocalDate.of(1975, 3, 7), new PhoneNumber("+32 65 123 456"),
 			new Location("Ho.23", "Houdain"));
