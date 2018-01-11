dbafc462a6cbc43e228a5706ded9185ccc2a82b1
mohamed-bahaj <Mohamed.BAHAJ@student.umons.ac.be>
2018-01-11 12:19:03 +0100

    final

--- src/main/java/exercise1/ExceptionCatcher.java
+++ src/main/java/exercise1/ExceptionCatcher.java
@@ -8,6 +8,8 @@ public class ExceptionCatcher {
 	}
 
 	void executeExceptionThrowerSafely(int value) {
-		exceptionThrower.throwACustomExceptionWhenValueIs42(value);
+		try {
+			exceptionThrower.throwACustomExceptionWhenValueIs42(value);
+		}catch (RuntimeException e){}
 	}
 }
--- src/main/java/exercise2/Exercise2.java
+++ src/main/java/exercise2/Exercise2.java
@@ -1,21 +1,29 @@
 package exercise2;
 
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDate;
 
+import static java.lang.System.out;
+
 public class Exercise2 {
-	public static void save(Classroom classroom, Path filePath) {
-
+	public static void save(Classroom classroom, Path filePath) throws IOException {
+		try(OutputStream out = Files.newOutputStream(filePath)){
+			ObjectOutput oos = new ObjectOutputStream(out);
+			oos.writeObject(classroom);
+		}
 	}
 
-	public static Classroom load(Path filePath) {
-		return null;
+	public static Classroom load(Path filePath) throws IOException, ClassNotFoundException {
+		try(InputStream in = Files.newInputStream(filePath)){
+			ObjectInput ois = new ObjectInputStream(in);
+			return (Classroom) ois.readObject();
+		}
 	}
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, ClassNotFoundException {
 		Teacher teacher = new Teacher("Claire", "Barnett",
 			LocalDate.of(1975, 3, 7), new PhoneNumber("+32 65 123 456"),
 			new Location("Ho.23", "Houdain"));
@@ -30,10 +38,10 @@ public class Exercise2 {
 
 		save(classroom, filePath);
 
-		System.out.printf("Classroom saved to %s, size=%d bytes\n", filePath.toString(), Files.size(filePath));
+		out.printf("Classroom saved to %s, size=%d bytes\n", filePath.toString(), Files.size(filePath));
 
 		Classroom loadedClassroom = load(filePath);
 
-		System.out.printf("Classroom loaded from %s: %s\n", filePath.toString(), loadedClassroom.toString());
+		out.printf("Classroom loaded from %s: %s\n", filePath.toString(), loadedClassroom.toString());
 	}
 }
